d82ea20807278cfd03afb067525befe720b0ef60
lab314-16@labeng.local <lab314-16@labeng.local>
2019-12-27 17:14:36 +0700

    lab3_3 Version one

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-    int number = 19;
+    int number = 69;
     float sum=0.0;
     float number1=1;
-    float number2=6;
+    float number2=2;
     while(number2<=number)
     {
         number1=1/number2;
