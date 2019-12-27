ea549a830494c8a653b95369b687711e3165b479
lab314-16@labeng.local <lab314-16@labeng.local>
2019-12-27 17:04:13 +0700

    First Version

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-using namespace std:
+using namespace std;
 
 int main()
 {
-    cout << "Hello"
-
+    int number = 69;
+    float sum=0.0;
+    float number1=1;
+    float number2=2;
+    while(number2<=number)
+    {
+        number1=1/number2;
+        number2 = number2+1;
+        sum = sum +number1;
+    }
+    cout << sum; 
     return 0;
 }
